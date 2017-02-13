4012c4be38afb88ab74bb3a5b5caece10d78f1fd
Ezra <ezrabathini@gmail.com>
2017-02-14 00:38:07 +1300

    basic get matches, populate tableView, err RecyclerView 👻😝😁

---
+++ app/src/main/java/com/ezrabathini/bowled/utilities/Match.java
@@ -0,0 +1,90 @@
+package com.ezrabathini.bowled.utilities;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ezra on 13/02/17.
+ */
+
+public class Match {
+    public Integer matchId;
+    public Integer seriesId;
+    public String seriesName;
+    public String matchName;
+    public String matchStatus;
+    public String matchType;
+    public Team homeTeam;
+    public Team awayTeam;
+    public Boolean isMultiday;
+    public Boolean isWomensMatch;
+    public Boolean hasScore;
+
+    public Match(Integer matchId, Integer seriesId, String matchStatus, Team homeTeam, Team awayTeam) {
+        this.matchId = matchId;
+        this.seriesId = seriesId;
+        this.matchStatus = matchStatus;
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+    }
+
+
+    public static ArrayList<Match> matchesFromJsonArray(JSONArray matchesArray) {
+        ArrayList<Match> matches = new ArrayList<Match>();
+//        Match[] matches = null;
+        if (matchesArray.length() > 0) {
+            for (int i=0; i < matchesArray.length(); i++) {
+                try {
+                    JSONObject matchObject = matchesArray.getJSONObject(i);
+                    Match match = Match.matchFromJsonObject(matchObject);
+                    matches.add(match);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+
+        return  matches;
+    }
+
+    static Match matchFromJsonObject(JSONObject matchObject) {
+        Match match = null;
+
+        try {
+            Integer matchId = matchObject.getInt("id");
+            String matchStatus = matchObject.getString("status");
+            String matchName = matchObject.getString("name");
+            String matchType = matchObject.getString("cmsMatchType");
+
+            JSONObject homeTeamObject = matchObject.getJSONObject("homeTeam");
+            JSONObject awayTeamObject = matchObject.getJSONObject("awayTeam");
+
+
+            Team homeTeam = Team.teamFromJSONObject(homeTeamObject);
+            Team awayTeam = Team.teamFromJSONObject(awayTeamObject);
+
+            JSONObject seriesObject = matchObject.getJSONObject("series");
+
+            Integer seriesId = seriesObject.getInt("id");
+            String seriesName = seriesObject.getString("name");
+
+            match = new Match(matchId, seriesId, matchStatus, homeTeam, awayTeam);
+            match.seriesName = seriesName;
+
+            //TODO rest of the match attributes
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return match;
+    }
+
+
+}
